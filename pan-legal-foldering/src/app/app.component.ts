b7cfa83981cdeca037c10f2aeaa837fea66c58f2
PabloSanchezLeyTR <pablo.sanchez2@thomsonreuters.com>
2024-06-03 11:55:11 -0600

    initial commit

---
+++ pan-legal-foldering/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { SafButton } from '@saffron/core-components';
+
+
+SafButton();
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less']
+})
+
+export class AppComponent {
+  title = 'pan-legal-foldering';
+}
+
