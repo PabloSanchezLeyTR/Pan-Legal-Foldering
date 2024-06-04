4037d9b306a49c25c2bbcc1ead9ac87a36104a05
PabloSanchezLeyTR <pablo.sanchez2@thomsonreuters.com>
2024-06-04 06:37:11 -0600

    Installing Saffron

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { SafButton } from '@saffron/core-components';
 
+SafButton();
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pan-legal-foldering';
+
 }
