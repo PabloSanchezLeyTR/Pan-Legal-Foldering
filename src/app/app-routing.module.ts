89a929e6532c254bee4e1803927b88ded57e2fd3
PabloSanchezLeyTR <pablo.sanchez2@thomsonreuters.com>
2024-06-10 12:41:27 -0600

    404 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,8 @@ const routes: Routes = [
   { path: 'research-history', component: ResearchHistoryComponent },
   { path: 'my-research', component: MyResearchComponent},
   {path: '404', component: ResearchHistoryComponent},
-  { path: '**', component: ResearchHistoryComponent }
+  { path: '**', component: ResearchHistoryComponent },
+  { path: '', component: ResearchHistoryComponent }
 ];
 
 @NgModule({
