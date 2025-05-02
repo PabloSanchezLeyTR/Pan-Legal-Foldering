a235a404af0a450760ac86fa5ef68ee28736ea97
axelCaballeroTR <Axel.Caballero@thomsonreuters.com>
2025-05-02 15:13:47 -0600

    progress p2

--- src/app/pages/westlaw-prototype/components/research/deep-research-result/deep-research-result.component.ts
+++ src/app/pages/westlaw-prototype/components/research/deep-research-result/deep-research-result.component.ts
@@ -10,12 +10,55 @@ export class DeepResearchResultComponent {
 
   loading: boolean = true;
   sources: number = 0;
+  fullReport: boolean = false;
+  preliminaryAnswer: boolean = false;
+  currentStep: number = 2;
+
 
   toggleLoading() {
     this.loading = !this.loading;
   }
 
+  resetStepData() {
+    this.loading = true;
+    this.sources = 0;
+    this.fullReport = false;
+    this.preliminaryAnswer = false;
+  }
+
   getSources() {
     this.sources = 12;
+  }
+
+  getPremilinaryAnswer() {
+    this.preliminaryAnswer = true;
+  }
+  getFullReport() {
+    this.fullReport = true;
+  }
+
+  setCurrentStep(step: number) {
+     /* steps
+    step 1 confirm research plan
+    step 2 loading, no sources
+    step 3 loading, sources ready (showing)
+    step 4 loading, preliminary answer, no full report
+    step 5 loading, full report
+  */
+
+    this.resetStepData();
+
+    this.currentStep = step;
+    if(step === 3) {
+      this.getSources();
+    }
+    if(step === 4) {
+      this.getSources();
+      this.getPremilinaryAnswer();
+    }
+    if(step === 5) {
+      this.getSources();
+      this.getFullReport();
+    }
   }
 }
