c3d75c5e9007e6a7f846c90b5b8514f70f67655f
axelCaballeroTR <Axel.Caballero@thomsonreuters.com>
2025-05-20 09:47:09 -0600

    more progress

--- src/app/pages/westlaw-prototype/components/research/deep-research-result/deep-research-result.component.ts
+++ src/app/pages/westlaw-prototype/components/research/deep-research-result/deep-research-result.component.ts
@@ -19,7 +19,37 @@ export class DeepResearchResultComponent {
   updateProgressBar() {
     if (this.progressBarIndicator) {
       const progressPercentage = (this.taskCurrent / this.taskTotal) * 100;
-      this.progressBarIndicator.nativeElement.style.width = `${progressPercentage}%`;
+      //this.progressBarIndicator.nativeElement.style.width = `${progressPercentage}%`;
+
+      switch (this.currentStep) {
+        case 2:
+          this.progressBarIndicator.nativeElement.style.width = '44px';
+          break;
+        case 3:
+          this.progressBarIndicator.nativeElement.style.width = '44px';
+          break;
+        case 4:
+          this.progressBarIndicator.nativeElement.style.width = '44px';
+          break;
+        case 5:
+          this.progressBarIndicator.nativeElement.style.width = '88px';
+          break;
+        case 6:
+          this.progressBarIndicator.nativeElement.style.width = '176px';
+          break;
+        case 7:
+          this.progressBarIndicator.nativeElement.style.width = '308px';
+          break;
+        case 8:
+          this.progressBarIndicator.nativeElement.style.width = '100%';
+          setTimeout(() => {
+              this.progressBarIndicator.nativeElement.style.background = '#387C2B';
+          }, 500);
+          break;
+        default:
+          // Default logic
+          break;
+      }
     }
   }
 
@@ -30,7 +60,7 @@ export class DeepResearchResultComponent {
   fullReport: boolean = false;
   preliminaryAnswer: boolean = false;
   currentStep: number = 2;
-  totalSteps: number = 5;
+  totalSteps: number = 8;
   taskCurrent: number = 1;
   taskTotal: number = 8;
   nQuestions: number = 0;
@@ -95,7 +125,7 @@ export class DeepResearchResultComponent {
 
   //advance the current step by 1
   nextStep() {
-    if(this.currentStep < 5) {
+    if(this.currentStep < 8) {
       this.setCurrentStep(this.currentStep + 1);
     }
   }
@@ -112,26 +142,55 @@ export class DeepResearchResultComponent {
 
     this.currentStep = step;
     if(step === 2) {
+      //no content visible
       this.taskCurrent = 1;
     }
 
     if(step === 3) {
-      this.nSources = 8;
+       //empty tabs and loader visible
       this.taskCurrent = 3;
     }
     if(step === 4) {
-      this.nSources = 8;
+      //preliminary answer and loader visible, 1 task completed, 1 question, 4 sources
+      this.nSources = 4;
       this.getPremilinaryAnswer();
       this.taskCurrent = 5;
-      this.nQuestions = 2;
+      this.nQuestions = 1;
     }
     if(step === 5) {
+      //preliminary answer and loader visible, 2 tasks completed, 1 question, 6 sources
+      this.nSources = 8;
+      this.taskCurrent = 8;
+      this.nQuestions = 5;
+      this.nNotes = 4;
+    }
+
+    if(step === 6) {
+      //preliminary answer and loader visible, at least 4 tasks completed, 1 question, 6 sources
+      this.nSources = 8;
+      this.taskCurrent = 8;
+      this.nQuestions = 5;
+      this.nNotes = 4;
+    }
+
+    if(step === 7) {
+      //preliminary answer and loader visible, seems all visible tasks completed, 1 question, 8 sources
+      this.nSources = 8;
+      this.taskCurrent = 8;
+      this.nQuestions = 5;
+      this.nNotes = 4;
+    }
+
+    if(step === 8) {
+      //no loader, full report, all visible tasks completed, 5 questions, 12 sources
       this.nSources = 8;
       this.getFullReport();
       this.taskCurrent = 8;
       this.nQuestions = 5;
       this.nNotes = 4;
+      this.researchReportTabLoading = false;
     }
+
 
     this.updateProgressBar()
   }
@@ -139,7 +198,7 @@ export class DeepResearchResultComponent {
   //create a fuction that advances the current step by 1 every 10 seconds
   advanceStep() {
     setTimeout(() => {
-      if(this.currentStep < 5) {
+      if(this.currentStep < 8) {
         this.setCurrentStep(this.currentStep + 1);
       }
     }, 2000);
